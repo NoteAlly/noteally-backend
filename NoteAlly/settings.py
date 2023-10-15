4695cb8109730d4722a4ee4367765c301993bc5f
Diogo Magalhães <d.magalhaes@ua.pt>
2023-10-15 19:00:29 +0100

    TEST-47 Changed compromised django key and keys are now kept in .env file, new requirements for loading .env variables and for postgres database connection

--- NoteAlly/settings.py
+++ NoteAlly/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ufqpg+(()yt)o74-@qtmzm7eup*38s%cxh8wl-8i(i#9581z^'
+SECRET_KEY = os.environ.get("django-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
