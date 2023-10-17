83435a288f5122fc9372018b99cabef7b5b901b9
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-10-17 10:46:31 +0100

    TEST-59 Add django models

--- noteally_app/models.py
+++ noteally_app/models.py
@@ -1,6 +1,56 @@
 from django.db import models
 
-# Create your models here.
-# class Professor(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100)
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_aws = models.IntegerField()
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    premium = models.BooleanField()
+    university = models.CharField(max_length=100)
+    karma_score = models.IntegerField()
+    study_areas = models.ManyToManyField('Study_Area', through='User_Study_Area')
+
+
+class Study_Area(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Resource(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField(default=0)
+    upload_date = models.DateTimeField()
+    university = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=100)
+    file_link = models.TextField()
+    study_areas = models.ManyToManyField(Study_Area, through='Resource_Study_Area')
+
+    @property
+    def total_downloads(self):
+        return Download.objects.filter(resource=self).count()
+    
+    @property
+    def total_likes(self):
+        return Like.objects.filter(resource=self, like=True).count()
+    
+    @property
+    def total_dislikes(self):
+        return Like.objects.filter(resource=self, like=False).count()
+
+
+class Download(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    download_date = models.DateTimeField()
+
+
+class Like(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    like = models.BooleanField()
